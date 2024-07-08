3ee11549ba10157bd9f4efc9a47b197c2424f0d8
amndalsr <laisamanda2015@gmail.com>
2024-07-08 20:49:13 -0300

    alinhamento populares

--- src/componentes/Galeria/Populares/index.jsx
+++ src/componentes/Galeria/Populares/index.jsx
@@ -1,7 +1,7 @@
 import Titulo from "../../Titulo";
 
 const Populares = () => {
-    return <Titulo>Populares</Titulo>; 
+    return <Titulo $alinhamento="center">Populares</Titulo>; 
 }
 
 export default Populares;   
