adecc227b5df3a143cc53dbce8a55d7ee7e8dc9c
amndalsr <laisamanda2015@gmail.com>
2024-07-08 18:19:08 -0300

    feat: itens da navegacao

--- src/componentes/BarraLateral/index.jsx
+++ src/componentes/BarraLateral/index.jsx
@@ -1,25 +1,53 @@
-import styled from "styled-components";
+import { styled } from "styled-components";
+import ItemNavegacao from "./ItemNavegacao";
 
 const ListaEstilizada = styled.ul`
-    list-style: none;
-    padding: 0;
-    margin: 0;
-`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  width: 212px;
+`;
 
 const BarraLateral = () => {
-    return (
-        <aside>
-            <nav>
-                <ListaEstilizada>
-                    <li><a href="#">Início</a></li>
-                    <li><a href="#">Mais vistas</a></li>
-                    <li><a href="#">Mais curtidas</a></li>
-                    <li><a href="#">Mais</a></li>
-                    <li><a href="#">Surpreenda-me</a></li>
-                </ListaEstilizada>
-            </nav>
-        </aside>
-    )
-}
+  return (
+    <aside>
+      <nav>
+        <ListaEstilizada>
+          <ItemNavegacao
+            iconeAtivo="/icones/home-ativo.png"
+            iconeInativo="/icones/home-inativo.png"
+            ativo={true}
+          >
+            Início
+          </ItemNavegacao>
+          <ItemNavegacao
+            iconeAtivo="/icones/mais-vistas-ativo.png"
+            iconeInativo="/icones/mais-vistas-inativo.png"
+          >
+            Mais vistas
+          </ItemNavegacao>
+          <ItemNavegacao
+            iconeAtivo="/icones/mais-curtidas-ativo.png"
+            iconeInativo="/icones/mais-curtidas-inativo.png"
+          >
+            Mais curtidas
+          </ItemNavegacao>
+          <ItemNavegacao
+            iconeAtivo="/icones/novas-ativo.png"
+            iconeInativo="/icones/novas-inativo.png"
+          >
+            Novas
+          </ItemNavegacao>
+          <ItemNavegacao
+            iconeAtivo="/icones/surpreenda-me-ativo.png"
+            iconeInativo="/icones/surpreenda-me-inativo.png"
+          >
+            Surpreenda-me
+          </ItemNavegacao>
+        </ListaEstilizada>
+      </nav>
+    </aside>
+  );
+};
 
 export default BarraLateral;
